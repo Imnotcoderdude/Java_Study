fb3e43414e1056ce6093fa3322a72e092e6405dd
Imnotcoderdude <topsolo7@naver.com>
2024-05-28 00:02:01 +0900

    codeKata : 행열의 덧셈 풀이.

--- src/CodeKata/noobForMac.java
+++ src/CodeKata/noobForMac.java
@@ -3,7 +3,7 @@ package CodeKata;
 import java.util.ArrayList;
 
 public class noobForMac {
-    class Solution {
+    class Solution32 {
         // 짝수의 값 더하기
         public int solution(int n) {
             int answer = 0;
@@ -70,7 +70,7 @@ public class noobForMac {
 
     }
 }
-class Solution {
+class Solution12 {
     public int solution(long num) {
         int answer = 0;
         int count = 0;
@@ -117,6 +117,58 @@ class Solution3 {
             answer = 1;
         } else {
             count = -1;
+        }
+        return answer;
+    }
+    class Solution {
+        public int[] solution(int[] arr) {
+            if (arr.length == 1) {
+                return new int[] {-1};
+            }
+            int min = arr[0];
+            for (int num : arr) {
+                if (num < min) {
+                    min = num;
+                }
+            }
+            int[] result = new int[arr.length - 1];
+            int index = 0;
+            for (int num : arr) {
+                if (num != min) {
+                    result[index++] = num;
+                }
+            }
+            return result;
+        }
+    }
+    class Solution1 {
+        public String solution(String s) {
+            int len = s.length();
+            int mid = len / 2;
+
+            if (len % 2 == 0) {
+                return s.substring(mid - 1, mid + 1);
+            } else {
+                return s.substring(mid);
+            }
+        }
+    }
+
+}
+// 5월 28일 코드카타 문제 풀이
+class solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int row = arr1.length; // 헹 개수
+        int col = arr1[0].length; // 열 개수
+        int row2 = arr2.length; // 비교를 위한 arr2 의 행 길이
+        int col2 = arr2[0].length; // 비교를 위한 arr2 의 열 길이
+        int[][] answer = new int[row][col]; // 결과를 담을 배열
+        if (row == row2 && col == col2) {
+            for (int i=0; i<row; i++) {
+                for (int j = 0; j < col; j++) {
+                    answer[i][j] = arr1[i][j] + arr2[i][j];
+                }
+            }
         }
         return answer;
     }
